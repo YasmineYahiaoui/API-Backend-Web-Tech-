fb4118dbdac4443cee881ca2031598d379c4bb40
YasmineYahiaoui <yahiaoui.yasmine11@gmail.com>
2024-11-06 20:15:09 -0500

    test

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize');
 
+// Remplacez 'database_name', 'username', et 'password' par vos informations
 const sequelize = new Sequelize('database_name', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql', // ou 'postgres', 'sqlite', selon votre configuration
+  host: 'localhost', // ou l'IP de votre serveur MySQL
+  dialect: 'mysql',
+  logging: false, // désactive le logging si vous ne voulez pas de détails sur les requêtes SQL
 });
 
 module.exports = sequelize;
