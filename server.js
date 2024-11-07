7121cc8a3ff1fe55202c45166584de8bfe7b2e1f
YasmineYahiaoui <yahiaoui.yasmine11@gmail.com>
2024-11-06 20:44:20 -0500

    database

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ sequelize.sync()
   });
 
 // Démarrer le serveur
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
